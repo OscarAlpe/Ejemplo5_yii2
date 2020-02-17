948036a8603f922ce65aeb9ca2f7e34af6e0ab88
Oscar Megía López <pfgytk@gmail.com>
2020-02-17 13:00:37 +0100

    Añadido que salga en el GridVierw la foto, el nombre del almacén y la dirección en dos líneas.

--- views/productos/index.php
+++ views/productos/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use app\models\Almacenes;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -25,13 +26,42 @@ $this->params['breadcrumbs'][] = $this->title;
 
             'id',
             'nombre',
-            'foto',
-            'almacen',
-            'fecha',
+            [ 'attribute' => 'foto',
+              'format'=>'html',
+              'label'=>'image',
+              'value'=>function($data) {
+                        return Html::img(yii\helpers\Url::base('http') . '/imgs/' . $data['foto'],
+                                ['width' => '80px',
+                                'height' => '80px']);
+                       },
+            ],
+            [
+                'attribute'=>'almacen',
+                'headerOptions' => ['style' => 'width:30%'],
+                'format' => 'html',
+                'value' => function ($data) {
+                    $output = '';
+                    $output .= $data->almacen . "<br />";
+                    $output .= Almacenes::findOne($data->almacen)->toArray()["nombre"] . "<br />";
+                    $output .= Almacenes::findOne($data->almacen)->toArray()["direccion"] . "<br />";
 
+                    return $output;
+                },
+            ],
+                        
+            'fecha',
+            //'almacen0.nombre',
+            /*
+             [
+                 'label' =>'Almacén 2',
+                 'format' => 'html',
+                 'value' =>function($d){
+                    return $d->almacen . "<br />" . $d->almacen0->nombre . "<br />" . $d->almacen0->direccion;
+                 }
+             ],
+             */
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
-
 
 </div>
